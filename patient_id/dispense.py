8861df01fc42d6bab11b45fb54a2789594e0c561
Victoria-lawal <104084402+Victoria-lawal@users.noreply.github.com>
2026-07-29 09:40:26 +0100

    Refactor comment for HAND_DETECT_THRESHOLD_MM

--- patient_id/dispense.py
+++ patient_id/dispense.py
@@ -6,7 +6,7 @@ import adafruit_vl53l0x
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_vl53l0x.VL53L0X(i2c)
 
-HAND_DETECT_THRESHOLD_MM = 60  # slightly above your tested 50mm, small margin for variation
+HAND_DETECT_THRESHOLD_MM = 60  # slightly above tested 50mm, small margin for variation
 STABLE_READS_REQUIRED = 5
 
 def wait_for_hand(timeout=15):
